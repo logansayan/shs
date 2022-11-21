6509e23418ff1c08f87b57b3fe26652de2dad0f9
logansayan <dassayan375@gmail.com>
2022-11-21 16:59:49 +0530

    add scrollbar to notices section

---
+++ js/noticesScroll.js
@@ -0,0 +1,14 @@
+const notices = document.querySelector(".notices__container");
+const noticeItem = document.querySelector(".notices__item");
+const up = document.querySelector(".scroll-up");
+const down = document.querySelector(".scroll-down");
+
+const itemHeight = noticeItem.offsetHeight + 25;
+
+down.addEventListener("click", () => {
+  notices.scrollBy(0, itemHeight);
+});
+
+up.addEventListener("click", () => {
+  notices.scrollBy(0, -itemHeight);
+});
