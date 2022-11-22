3f9d40a00adbeb9efa4deacc45c3f2c9e72fa186
logansayan <dassayan375@gmail.com>
2022-11-22 11:41:09 +0530

    fix footer for phone size

--- js/noticesScroll.js
+++ js/noticesScroll.js
@@ -3,7 +3,7 @@ const noticeItem = document.querySelector(".notices__item");
 const up = document.querySelector(".scroll-up");
 const down = document.querySelector(".scroll-down");
 
-const itemHeight = noticeItem.offsetHeight + 25;
+const itemHeight = noticeItem.offsetHeight + 24;
 
 down.addEventListener("click", () => {
   notices.scrollBy(0, itemHeight);
